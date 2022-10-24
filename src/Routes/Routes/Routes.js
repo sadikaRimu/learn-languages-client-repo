f28e037b9be9e4c6836496b4bb28fd535398f258
sadikaRimu <sadika3046@gmail.com>
2022-10-25 01:23:22 +0600

    route page added

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,11 +1,48 @@
 import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import CourseCategory from '../../components/CourseCategory/CourseCategory';
+import Courses from '../../components/Courses/Courses';
+import Home from '../../components/Home/Home';
+import Login from '../../components/Login/Login/Login';
+import Register from '../../components/Login/Register/Register';
+import Main from '../../layout/Main';
 
-const Routes = () => {
-    return (
-        <div>
-
-        </div>
-    );
-};
-
-export default Routes;
+export const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>,
+                // loader: () => fetch('http://localhost:5000/news')
+            },
+            {
+                path: '/courseCategory/:id',
+                element: <CourseCategory></CourseCategory>,
+                // loader: ({ params }) => fetch(`http://localhost:5000/category/${params.id}`)
+            },
+            {
+                path: '/courses/:id',
+                element: <Courses></Courses>,
+                // loader: ({ params }) => fetch(`http://localhost:5000/news/${params.id}`)
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            }
+            // {
+            //     path: '/terms',
+            //     element: <TermsAndConditions></TermsAndConditions>
+            // },
+            // {
+            //     path: '/profile',
+            //     element: <PrivateRoute><Profile></Profile></PrivateRoute>
+            // }
+        ]
+    }
+]) 
