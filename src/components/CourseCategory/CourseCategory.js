5949844b4139729ecd9093f99da485b0b37c5528
sadikaRimu <sadika3046@gmail.com>
2022-10-25 01:18:37 +0600

    components added

---
+++ src/components/CourseCategory/CourseCategory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CourseCategory = () => {
+    return (
+        <div>
+            <h2>course category page</h2>
+        </div>
+    );
+};
+
+export default CourseCategory;
